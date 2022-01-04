6d139058a1b8e89700d26531ff564df2f7018c8d
lyf <405781809@qq.com>
2022-01-04 22:19:03 +0800

    编码解码

--- codec/tiny_codec.cxx
+++ codec/tiny_codec.cxx
@@ -1,1 +1,11 @@
 #include "tiny_codec.h"
+
+Buffer CommonCodec::encode(const Msg& msg)
+{
+
+}
+
+Msg CommonCodec::decode(char* buf, size_t len)
+{
+    
+}
--- include/tiny_codec.h
+++ include/tiny_codec.h
@@ -2,17 +2,35 @@
 #define __CODEC_H__
 
 #include <cstddef>
+#include <tiny_buffer.h>
+
+struct MsgHead
+{
+    unsigned short m_msgLen;  //消息长度(包头+包体)
+    unsigned short m_type;    //消息类型
+    int m_crc32;
+};
 
 struct Msg
 {
-
+    MsgHead m_msgHead;
+    Buffer  m_msgBuffer;
 };
 
 struct CodeBase
 {
-    virtual void encode(const Msg& msg) = 0;
-    virtual Msg decode(char* buf, size_t len) = 0;
+    virtual Buffer encode(const Msg& msg) = 0;     //编码消息
+    virtual Msg decode(char* buf, size_t len) = 0; //解码消息
     virtual ~CodeBase() {}
+};
+
+struct CommonCodec:public CodeBase
+{
+    Buffer encode(const Msg& msg);
+    Msg decode(char* buf, size_t len);
+
+private:
+
 };
 
 #endif
