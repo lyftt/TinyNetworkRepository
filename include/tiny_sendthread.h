51918fa0fb1fc7c394cb4219c988d9a8c9288da8
lyf <405781809@qq.com>
2022-01-18 23:40:49 +0800

    修改发送线程代码

--- include/tiny_sendthread.h
+++ include/tiny_sendthread.h
@@ -1,6 +1,44 @@
 #ifndef __SEND_THREAD_H__
 #define __SEND_THREAD_H__
 
+#include <memory>
+#include <atomic>
+#include <mutex>
+#include <list>
+#include <semaphore.h>
+#include "tiny_codec.h"
 
+struct SendThread: public std::enable_shared_from_this<SendThread>
+{
+    ~SendThread();
+
+    //禁止拷贝和移动
+    SendThread(const SendThread&) = delete;
+    SendThread& operator=(const SendThread&) = delete;
+    SendThread(SendThread&&) = delete;
+    SendThread& operator=(SendThread&&) = delete;
+
+    //停止发送线程
+    void stopThread();
+    //放入消息
+    bool pushMsg(Msg&& msg);
+
+    //工厂函数，创建一个发送线程
+    static std::shared_ptr<SendThread> createThread();
+
+private:
+    //线程工作函数
+    static void workThread(std::weak_ptr<SendThread> sendThread);
+
+private:
+    SendThread();
+
+    //线程停止标志
+    std::atomic<bool> m_stop;
+    std::atomic<size_t> m_msgEventQueueSize;
+    sem_t             m_semEventQueue;
+    std::mutex        m_mtxEventQueue;
+    std::list<Msg>    m_msgEventQueue;
+};
 
 #endif
--- net/tiny_sendthread.cxx
+++ net/tiny_sendthread.cxx
@@ -1,10 +1,85 @@
 #include "tiny_sendthread.h"
+#include <thread>
 
-struct SendThread
+SendThread::SendThread():m_stop(false), m_msgEventQueueSize(0)
 {
-    SendThread();
-    ~SendThread();
+    sem_init(&m_semEventQueue, 0, 0);   //可能失败
+}
 
-private:
+SendThread::~SendThread()
+{
+    m_stop = true;
+    sem_destroy(&m_semEventQueue);   //可能失败
+}
 
-};
+std::shared_ptr<SendThread> SendThread::createThread()
+{
+    std::shared_ptr<SendThread> sp(new SendThread);
+    std::weak_ptr<SendThread> wp(sp);
+    std::thread th(SendThread::workThread, wp);
+    return sp;
+}
+
+void SendThread::stopThread()
+{
+    m_stop = true;
+}
+
+bool SendThread::pushMsg(Msg&& msg)
+{
+    std::lock_guard<std::mutex> guard(m_mtxEventQueue);
+    m_msgEventQueue.emplace_back(std::move(msg));
+    ++m_msgEventQueueSize;
+    sem_post(&m_semEventQueue);
+    return true;
+}
+
+void SendThread::workThread(std::weak_ptr<SendThread> sendThread)
+{
+    while(true)
+    {
+        std::shared_ptr<SendThread> sp = sendThread.lock();
+
+        if(!sp)
+        {
+            return;
+        }
+
+        if(sp->m_stop)
+        {
+            return;
+        }
+
+        //等待信号
+        if(sem_wait(&sp->m_semEventQueue) == -1)
+        {
+            if(errno == EINTR)
+            {
+                continue;
+            }
+            //出错了
+            else
+            {
+                sp->m_stop = true;  //停止吧
+                return;
+            }
+        }
+
+        if(sp->m_stop)
+        {
+            return;
+        }
+
+        if(sp->m_msgEventQueueSize > 0)
+        {
+            Msg msg;
+
+            {
+                std::lock_guard<std::mutex> guard(sp->m_mtxEventQueue);
+                msg = std::move(sp->m_msgEventQueue.front());
+                sp->m_msgEventQueue.pop_front();
+
+            }
+        }
+    }
+}
